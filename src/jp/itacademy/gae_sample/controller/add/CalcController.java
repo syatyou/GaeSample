51314baf6e48f78ccebf002976793da7cbb51c5c
itacademy <itacademy@192.168.1.11>
2012-11-10 21:27:59 +0900

    tukareta

--- src/jp/itacademy/gae_sample/controller/add/CalcController.java
+++ src/jp/itacademy/gae_sample/controller/add/CalcController.java
@@ -2,11 +2,22 @@ package jp.itacademy.gae_sample.controller.add;
 
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.controller.validator.Validators;
 
 public class CalcController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
+        
+        Validators v = new Validators(request);
+        v.add("num1", v.required(), v.integerType());
+        v.add("num2", v.required(), v.integerType());
+        v.
+        
+        if (!v.validate())  {
+            return forward("index.jsp");
+        }
+        
         int num1 = asInteger("num1");
         int num2 = asInteger("num2");
         int result = num1 + num2;
